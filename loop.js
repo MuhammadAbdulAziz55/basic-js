5dccd14fe34fafdee218e9156f9dc332d2987e1c
MuhammadAbdulAziz55 <76765396+MuhammadAbdulAziz55@users.noreply.github.com>
2022-03-31 21:12:40 +0600

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,31 @@
+// While Loop
+var n = 0;
+
+while (n < 3) {
+    // console.log(n);
+     n++;
+}
+// expected output: 0 1 2 
+
+let n1  = 10;
+while (n1 <= 15){
+    // console.log(n1);
+    n1++;
+}
+//  expected output: 10 11 12 13 14 15
+
+let n2 = 0;
+while ( n2 < 3) {
+    n2++;
+};
+// console.log(n2);
+//  expected output: 3
+
+
+let n3 = 10;
+while (n3 <= 15){
+    n3++;
+};
+console.log(n3);
+//  expected output: 16
+
