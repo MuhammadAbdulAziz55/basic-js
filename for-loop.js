4c6b7334191d1501f8d2298ece5cd72954c744e5
MuhammadAbdulAziz55 <76765396+MuhammadAbdulAziz55@users.noreply.github.com>
2022-04-06 12:30:33 +0600

    Add files via upload

---
+++ for-loop.js
@@ -0,0 +1,24 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for(let i = 0; i < 10; i++ ){
+    // console.log(i);
+}
+
+
+var nums = [22, 33, 44, 55, 66, 77];
+
+for(i=0; i< nums.length; i++){
+    var element = nums[i];
+    // console.log(element);
+}
+
+
+var friends = ['Golam Rabbani', 'Riaz', 'Azizul Haque', 'Rakib'];
+
+for(i = 0; i < friends.length; i++){
+    var element = friends[i];
+    console.log(element);
+}
---
+++ function.js
@@ -0,0 +1,43 @@
+function sayLoveYou() {
+    // console.log('Jaan o baby');
+    // console.log('Billy baby....Koi tumi');
+    
+}
+sayLoveYou();
+
+var date = 14;
+var place = 'Restaurant';
+sayLoveYou();
+var hangoutPlace = "TSC";
+sayLoveYou();
+
+
+// function doubleIt(num){
+    // var result = num * 2;
+    // console.log(result)
+// }
+
+// doubleIt(5);
+// doubleIt(9);
+// doubleIt(50);
+
+// Ex
+ function tripleThis(num2) {
+     var result = num2 * 3;
+     return result;
+ }
+
+ var first = tripleThis(20);
+ var second = tripleThis(30);
+ var total = first + second;
+//  console.log(total);
+
+function add(num3, num4){
+    var result = num3 + num4;
+    return result;
+}
+
+var sum = add(44, 56);
+console.log(sum);
+
+
